ed597dbac57c1809300844a0c1c1df2828e3aaf1
DANIELCARMEL <danielcarmel6@gmail.com>
2018-10-09 02:18:53 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// progress bar
+$(document).ready(function(){
+    $(document).on("scroll", function(){
+        var barTotal = ($(window).height() * 2) + ($(window).width() * 2)
+        var scrollTotal = $(document).height() - $(window).height()
+        var pixels = $(document).scrollTop()
+        var pc = pixels / scrollTotal
+        var barAcross = $(window).width() / barTotal
+        var barDown = $(window).height() / barTotal
+        var bar1 = pc / barAcross
+        var bar2 = (pc - barAcross) / barDown
+        var bar3 = (pc - barAcross - barDown) / barAcross
+        var bar4 = (pc - barAcross - barDown - barAcross) / barDown
+        $("div.bar-1").css("width",  100 * bar1 + "%")
+        $("div.bar-2").css("height", 100 * bar2 + "%")
+        $("div.bar-3").css("width",  100 * bar3 + "%")
+        $("div.bar-4").css("height", 100 * bar4 + "%")
+    });
+    
+    // Jump to hire section
+    $("#bar-home, #bar-about, #bar-projects, #bar-contact").click(function() {
+        var href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, 2000);
+        return false
+    });
+    
+});
