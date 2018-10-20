32284e586dfcbcc68840810f0631e828d9ea0fc8
DANIELCARMEL <danielcarmel6@gmail.com>
2018-10-20 21:45:07 +0300

    sdffkgl

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 $(document).ready(function(){
     // progress bar
     $(document).on("scroll", function(){
-        var barTotal = ($(window).height() * 2) + ($(window).width() * 2)
-        var scrollTotal = $(document).height() - $(window).height()
-        var pixels = $(document).scrollTop()
-        var pc = pixels / scrollTotal
-        var barAcross = $(window).width() / barTotal
-        var barDown = $(window).height() / barTotal
-        var bar1 = pc / barAcross
-        var bar2 = (pc - barAcross) / barDown
-        var bar3 = (pc - barAcross - barDown) / barAcross
-        var bar4 = (pc - barAcross - barDown - barAcross) / barDown
-        $("div.bar-1").css("width",  100 * bar1 + "%")
-        $("div.bar-2").css("height", 100 * bar2 + "%")
-        $("div.bar-3").css("width",  100 * bar3 + "%")
-        $("div.bar-4").css("height", 100 * bar4 + "%")
+        //var barTotal = $(window).width()
+        //var scrollTotal = $(document).height() - $(window).height()
+        //var pixels = $(document).scrollTop()
+        //var pc = ($(document).scrollTop()) / ($(document).height() - $(window).height())
+        //var barAcross = $(window).width() / ($(window).width())
+        var bar = (($(document).scrollTop()) / ($(document).height() - $(window).height())) / ($(window).width() / ($(window).width()))
+        $("div.bar").css("width",  100 * bar + "%")
     });
 
 });
