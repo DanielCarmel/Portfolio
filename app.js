99934e03f828993eb3cb052eaa0f75a29ee5e91d
DANIELCARMEL <danielcarmel6@gmail.com>
2018-10-22 00:23:43 +0300

    form added need to costumize

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 var path = require('path')
 const port = process.env.PORT || 3000
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(port, function(){
     console.log('listening to port %d', port);
 })
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.post('/submit', function(res, req){
+    console.log(res.body.lastName)
+    //https://freefrontend.com/css-forms/
+})
